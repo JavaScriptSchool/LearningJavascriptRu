659f8c03feb499ba07c6d0398a7bcb16c1c4d805
rsamokhin <poshliemail@googlemail.com>
2015-01-17 02:43:08 +0300

    date format

--- Learning.Javascript.ru/021.date.create.js
+++ Learning.Javascript.ru/021.date.create.js
@@ -1,2 +1,5 @@
 console.log(new Date(2012,1,20));
 console.log(new Date(1000*3600*24*(365)*42+1000*3600*24*(50+(2012-1970)/4)));
+
+var a = new Date;
+console.log( ('0'+a.getDate()).slice(-2)  + '.'  + ('0'+(a.getMonth())+1).slice(-2)    +'.' + (a.getFullYear()).toString().slice(-2));
