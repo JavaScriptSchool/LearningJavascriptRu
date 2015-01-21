9ab6f66184fc94dc676c5f60f658c1c964785c58
rsamokhin <poshliemail@googlemail.com>
2015-01-21 18:29:27 +0300

    recursive closures sum added

--- Learning.Javascript.ru/022.closures.sum.js
+++ Learning.Javascript.ru/022.closures.sum.js
@@ -1,10 +1,27 @@
 /**
  * Created by rsamokhin on 21.01.15.
  */
-var t = sum(5)(6)
+var t = sum(5)(6);
 console.log(t);
 function sum(a){
     return function (b){
         return a+b;
     }
+}
+
+/**
+ * Created by rsamokhin on 21.01.15.
+ */
+var tRecursive = sumRecursive(5)(6)(5)(7);
+console.log(tRecursive);
+function sumRecursive(a){
+    c = a;
+    function f(b){
+        c+=b;
+        return f;
+    }
+    f.toString = function(){
+        return c;
+    }
+    return f;
 }
