0b7ed98588de8c3cf0c66b0d9fe77388072fa0c7
rsamokhin <poshliemail@googlemail.com>
2015-01-23 01:03:26 +0300

    army of shooters

---
+++ Learning.Javascript.ru/027.closures.army.of.shooters.js
@@ -0,0 +1,16 @@
+function makeArmy(){
+    var shooters = [];
+    for (var i =0 ; i < 10 ; i++){
+
+        var shooter = function me(){
+            console.log(me.i)
+        }
+        shooter.i = i;
+        shooters.push(shooter);
+    }
+    return shooters;
+}
+var army = makeArmy();
+army[0]();
+army[9]();
+army[5]();
