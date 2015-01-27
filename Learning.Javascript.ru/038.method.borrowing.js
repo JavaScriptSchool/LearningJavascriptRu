a69e301fb0243cdaa3dbaf12501d787203dc85b3
rsamokhin <poshliemail@googlemail.com>
2015-01-27 23:50:31 +0300

    method borrowing, apply and call

---
+++ Learning.Javascript.ru/038.method.borrowing.js
@@ -0,0 +1,20 @@
+function joinArgs(){
+    arguments.join = [].join;
+    return arguments.join('.');
+}
+console.log(joinArgs());
+console.log(joinArgs(1,2,5,'2312'));
+
+function joinCall(){
+    var join = [].join;
+    return join.call(arguments,'-');
+}
+console.log(joinCall());
+console.log(joinCall(1,2,5,'2312'));
+
+function joinApply(){
+    var join = [].join;
+    return join.apply(arguments,['-']);
+}
+console.log(joinApply());
+console.log(joinApply(1,2,5,'2312'));
