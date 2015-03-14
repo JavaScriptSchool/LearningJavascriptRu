79c0857351283e37715d9bc562f67212e5ef5587
rsamokhin <poshliemail@googlemail.com>
2015-03-14 14:00:00 +0400

    console.table

---
+++ Learning.Javascript.ru/082.console.table.js
@@ -0,0 +1,16 @@
+/**
+ * Created by rsamokhin on 14.03.15.
+ */
+var languages = [
+        {
+            abb:'eng',
+            name:'English'
+        },{
+            abb:'ru',
+            name:'Russian'
+        },{
+            abb:'es',
+            name:'Spanish'
+        }
+    ];
+console.table(languages);
