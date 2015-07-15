1e2e30e50507fd5d3b34bc2cebe256411174036d
rSamokhin <poshliemail@googlemail.com>
2015-07-15 20:01:28 +0300

    es 6 classes intro

---
+++ Learning.Javascript.ru/097.class.es6.introduction.js
@@ -0,0 +1,12 @@
+'use strict';
+class Point {
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+    }
+    toString() {
+        return '(' + this.x + ', '+ this.y + ')';
+    }
+}
+var p = new Point(1,2);
+console.log(p);
