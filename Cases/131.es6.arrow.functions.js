3ffde8b8db57ce4ad7658886cd05dac559cf29eb
rSamokhin <poshliemail@googlemail.com>
2016-04-14 16:02:16 +0300

    arrow lol

---
+++ Cases/131.es6.arrow.functions.js
@@ -0,0 +1,11 @@
+function countdown (n) {
+    console.log('begin');
+    while (n --> 0) {
+        console.log(n);
+    }
+    console.log('end');
+}
+countdown(10);
+
+console.log('=========================================');
+
