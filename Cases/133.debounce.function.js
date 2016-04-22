f04597dddbfb9c5b2c5dced5da84d81c7249e35a
rSamokhin <poshliemail@googlemail.com>
2016-04-22 14:30:14 +0300

    debounce func

---
+++ Cases/133.debounce.function.js
@@ -0,0 +1,31 @@
+// Возвращает функцию, которая не будет срабатывать, пока продолжает вызываться.
+// Она сработает только один раз через N миллисекунд после последнего вызова.
+// Если ей передан аргумент `immediate`, то она будет вызвана один раз сразу после
+// первого запуска.
+
+function debounce (func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this,
+            args = arguments,
+            later = function () {
+                timeout = null;
+                if (!immediate) {
+                    func.apply(context, args);
+                }
+            },
+            callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) {
+            func.apply(context, args);
+        }
+    };
+}
+
+
+//USAGE
+var myEfficientFN = debounce( function () {
+    console.log('scroll');
+}, 250);
+window.addEventListener('resize', myEfficientFN);
